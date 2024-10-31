f0e46076db713f8cb2485ffd40d692d65674033e
karimsteros <karimbenamar49@gmail.com>
2024-10-31 10:41:32 +0100

    Your commit message here

---
+++ UDPClient.java
@@ -0,0 +1,32 @@
+// UDPClient.java
+
+// Import the networking libraries
+package udp_s_c;
+
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
+public class UDPClient {
+
+  public static void main(String[] args) throws IOException {
+    DatagramPacket sendPacket;
+    byte[] sendData;
+    // Create a Datagram Socket
+    DatagramSocket clientSocket = new DatagramSocket();
+    // Set client timeout to be 1 second
+    clientSocket.setSoTimeout(1000);
+    Scanner input = new Scanner(System.in);
+    while (true) {
+      String cmd = input.nextLine();
+      // If client types quit, close the socket and exit
+      if (cmd.equals("QUIT")) {
+        clientSocket.close();
+        System.exit(1);
+      }
+      sendData = cmd.getBytes();
+      sendPacket = new DatagramPacket(sendData, sendData.length, InetAddress.getByName("127.0.0.1"), 5001);
+      clientSocket.send(sendPacket);
+    }
+  }
+}
